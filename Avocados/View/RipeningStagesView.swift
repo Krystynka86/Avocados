fd3eabae54af0813b0e7537133357c5d94216b85
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-08-18 17:53:50 +0200

    add tab view

---
+++ Avocados/View/RipeningStagesView.swift
@@ -0,0 +1,20 @@
+//
+//  RipeningStagesView.swift
+//  Avocados
+//
+//  Created by Cristina on 2023-08-18.
+//
+
+import SwiftUI
+
+struct RipeningStagesView: View {
+    var body: some View {
+        Text("Ripening Stages")
+    }
+}
+
+struct RipeningStagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        RipeningStagesView()
+    }
+}
---
+++ Avocados/View/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Avocados
+//
+//  Created by Cristina on 2023-08-18.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
