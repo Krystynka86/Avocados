9a405921f90b4d2e5b4ec488498d5e1a371a6214
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-08-18 13:56:49 +0200

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Cristina on 2023-08-18.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
