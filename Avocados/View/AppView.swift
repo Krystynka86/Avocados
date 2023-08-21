b6e0a7b63167314044c70e8143b47996aa3f5c4d
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-08-21 21:58:35 +0200

    create avocados view

--- Avocados/View/AppView.swift
+++ Avocados/View/AppView.swift
@@ -8,36 +8,37 @@
 import SwiftUI
 
 struct AppView: View {
-    var body: some View {
-        TabView {
-            AvocadosView()
-                .tabItem ({
-                    Image("tabicon-branch")
-                    Text("Avocados")
-            })
-            ContentView()
-                .tabItem ({
-                    Image("tabicon-book")
-                    Text("Recipes")
-                })
-            RipeningStagesView()
-                .tabItem ({
-                    Image("tabicon-avocado")
-                    Text("Ripening")
-            })
-            SettingsView()
-                .tabItem ({
-                    Image("tabicon-settings")
-                    Text("Settings")
-            })
-        }
-        
-        .accentColor(Color.primary)
+  var body: some View {
+    TabView {
+      AvocadosView()
+        .tabItem({
+          Image("tabicon-branch")
+          Text("Avocados")
+        })
+      ContentView()
+        .tabItem({
+          Image("tabicon-book")
+          Text("Recipes")
+        })
+      RipeningStagesView()
+        .tabItem({
+          Image("tabicon-avocado")
+          Text("Ripening")
+        })
+      SettingsView()
+        .tabItem({
+          Image("tabicon-settings")
+          Text("Settings")
+        })
     }
+    .accentColor(Color.primary)
+  }
 }
+
 
 struct AppView_Previews: PreviewProvider {
     static var previews: some View {
         AppView()
+            .environment(\.colorScheme, .dark)
     }
 }
