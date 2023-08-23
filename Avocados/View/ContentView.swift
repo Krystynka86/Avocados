a0bbde035d234c8316d55d42f119a5252952089a
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-08-23 20:54:09 +0200

    add recipe model and data

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     // MARK: - PROPERTIES
     
     var headers: [Header] = headersData
+    var facts: [Fact] = factsData
     
     
     // MARK: - BODY
@@ -34,6 +35,23 @@ struct ContentView: View {
                 
                 DishesView()
                     .frame(maxWidth: 640) // for IPad
+                
+                // MARK: - AVOCADO FACTS
+                
+                Text("Avocado Facts")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 60) {
+                        ForEach(facts) { item in
+                            FactsView(fact: item)
+                        }
+                    } //: HStack
+                    .padding(.vertical)
+                    .padding(.leading, 60)
+                    .padding(.trailing, 20)
+                }
                 
                 // MARK: - FOOTER
                 
@@ -71,6 +89,6 @@ struct TitleModifier: ViewModifier {
 // MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(headers: headersData)
+        ContentView(headers: headersData, facts: factsData)
     }
 }
